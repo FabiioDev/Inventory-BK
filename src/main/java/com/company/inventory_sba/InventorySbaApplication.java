3be94917dea0b9e25c1aedbc8c34801166f931b4
FabiioDev <faboordo@gmail.com>
2024-12-17 10:48:24 -0600

    Proyecto base de control de inventarios

---
+++ src/main/java/com/company/inventory_sba/InventorySbaApplication.java
@@ -0,0 +1,13 @@
+package com.company.inventory_sba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorySbaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventorySbaApplication.class, args);
+    }
+
+}
